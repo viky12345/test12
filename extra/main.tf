761177e90e931cc3f694874a59c3f4b2f41febea
viky12345 <vvicky4934@gmail.com>
2022-11-13 22:47:43 +0530

    upda

--- extra/main.tf
+++ extra/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "das-ec2" {
     ami = var.ami
     instance_type = var.instance_type
     subnet_id = "subnet-5e7a9a23"
-    vpc_security_group_ids = "sg-6e0a4a1c"
+    vpc_security_group_ids = ["sg-6e0a4a1c"]
     monitoring = "true"
     user_data = file("${path.module}/install-ssm.sh")
 
